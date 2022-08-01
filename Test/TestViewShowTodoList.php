9382e13845c4752fc2c3648b93ac438b4341d3bd
Yrr3L <lerryjoostensz@gmail.com>
2022-08-01 23:33:27 +0800

    test view viewshowTodolost

---
+++ Test/TestViewShowTodoList.php
@@ -0,0 +1,13 @@
+<?php
+require_once "../View/ViewShowTodoList.php";
+require_once "../BusinessLogic/AddTodoList.php";
+
+addTodoList("Lerry");
+addTodoList("Julian");
+addTodoList("Joostensz");
+addTodoList("Aku");
+addTodoList("ESC17");
+
+
+
+viewShowTodoList();
