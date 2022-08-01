2efc1b0de8d644393348265c86f3d4f19ca3c11d
Yrr3L <lerryjoostensz@gmail.com>
2022-08-01 23:55:04 +0800

    make viewaddTodolost

--- View/ViewAddTodoList.php
+++ View/ViewAddTodoList.php
@@ -1,5 +1,16 @@
 <?php
+require_once "../Model/TodoList.php";
+require_once "../BusinessLogic/AddTodoList.php";
+require_once "../Helper/Input.php";
+
 
 function viewAddTodoList()
 {
+    echo "MENAMBAH TODO" . PHP_EOL;
+
+    $todo = input("Todo (x untuk batal) : ");
+    if ($todo == "x") {
+    } else {
+        addTodoList($todo);
+    }
 }
