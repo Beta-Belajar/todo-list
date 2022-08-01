68b04d33381d3ee703f33ff8a5d4121cb9ae62ee
Yrr3L <lerryjoostensz@gmail.com>
2022-08-02 00:11:46 +0800

    make viewRemoveTodolost

--- View/ViewRemoveTodoList.php
+++ View/ViewRemoveTodoList.php
@@ -1,5 +1,21 @@
 <?php
+require_once "../BusinessLogic/RemoveTodoList.php";
+require_once "../BusinessLogic/RemoveTodoList.php";
 
 function viewRemoveTodoList()
 {
+    echo "MENGHAPUS TODO" . PHP_EOL;
+    $pilihan = input("Nomor (x untuk batalkan)");
+
+    if ($pilihan == "x") {
+        echo "Batal Menghapus TODO";
+    } else {
+        $success = removeTodoList($pilihan);
+
+        if ($success) {
+            echo "Berhasil Menghapus TODO Nomor - $pilihan" . PHP_EOL;
+        } else {
+            echo "Gagal Menghapus TODO Nomor - $pilihan" . PHP_EOL;
+        }
+    }
 }
